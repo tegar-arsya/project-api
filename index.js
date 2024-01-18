6f407262848a1c54e32639b3efde28c592316c35
tegar-arsya <tegararsya0117@gmail.com>
2024-01-18 07:15:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Konfigurasi multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/'); // Folder penyimpanan file
+    },
+    filename: function(req, file, cb) {
+        const ext = path.extname(file.originalname);
+        cb(null, Date.now() + ext); // Nama file baru dengan timestamp
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Contoh data
+const items = [{
+        id: 1,
+        name: 'Personal Website',
+        imageUrl: 'portfolio_v2',
+        isi: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
+        ket: 'VueJS 3, Tailwind',
+    },
+    // Tambahkan data lainnya
+];
+
+// Mendapatkan semua item
+app.get('/api/items', (req, res) => {
+    res.json(items);
+});
+
+// Menambahkan item baru dengan gambar
+app.post('/api/items', upload.single('imageUrl'), (req, res) => {
+    const newItem = req.body;
+    newItem.id = items.length + 1;
+
+    if (req.file) {
+        // Jika ada file yang diunggah, tambahkan properti imageUrl ke newItem
+        newItem.imageUrl = `http://localhost:${port}/uploads/${req.file.filename}`;
+    }
+
+    items.push(newItem);
+    res.json(newItem);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
